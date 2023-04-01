be3335d28e69487a693726fa936e0a758142ce72
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2023-03-31 21:57:31 -0300

    Content Negotiation via HEADER Parameter

--- java-erudio/src/main/java/br/com/augusto/config/WebConfig.java
+++ java-erudio/src/main/java/br/com/augusto/config/WebConfig.java
@@ -12,13 +12,22 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
         //Como acessar os metodos pelo postman:
-        //localhost:8080/api/person/v1/?mediaType=xml
-        configurer.favorParameter(true)
-                .parameterName("mediaType").ignoreAcceptHeader(true)
-                .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                    .mediaType("json",MediaType.APPLICATION_JSON )
-                    .mediaType("xml",MediaType.APPLICATION_XML );
+        // VIA QUERY PARAM. localhost:8080/api/person/v1/?mediaType=xml
+        //configurer.favorParameter(true)
+       //         .parameterName("mediaType").ignoreAcceptHeader(true)
+       //         .useRegisteredExtensionsOnly(false)
+       //         .defaultContentType(MediaType.APPLICATION_JSON)
+       //             .mediaType("json",MediaType.APPLICATION_JSON )
+       //             .mediaType("xml",MediaType.APPLICATION_XML );
+
+
+        //VIA HEADER PARAM. localhost:8080/api/person/v1/
+        configurer.favorParameter(false)
+                 .ignoreAcceptHeader(false)
+                 .useRegisteredExtensionsOnly(false)
+                 .defaultContentType(MediaType.APPLICATION_JSON)
+                     .mediaType("json",MediaType.APPLICATION_JSON )
+                     .mediaType("xml",MediaType.APPLICATION_XML );
 
     }
 }
