9933d6a8b46ac848520481f2dfb512c827096624
Augusto Fernandes <augusto.fcosta1808@gmail.com>
2023-04-10 13:35:19 -0300

    Definindo a Configuração Básica do Swagger (Open API)

---
+++ java-erudio/src/main/java/br/com/augusto/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package br.com.augusto.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return  new OpenAPI()
+                .info(new Info()
+                        .title("RESTfull API with Java 18 and Spring Boot 3")
+                        .version("v1")
+                        .description("projeto spring ")
+                        .termsOfService("https://testeswagger.com.br")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://testeswagger.com.br")));
+    }
+}
